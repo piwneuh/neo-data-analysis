aab44b9bc89e29db3fad876d8f5e4718b9c27d1a
Filip Pinjuh <filip.pinjuh@gmail.com>
2023-04-26 21:34:26 +0200

    feat: more formatting

--- batch_processor/run.py
+++ batch_processor/run.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
 
 def main():
     spark = (
@@ -15,8 +16,26 @@ def main():
     print('reading source')
     df = spark.read.csv(
         source_path, inferSchema=True, header=True)
-    # TODO: Format the dataset and write it to a postgres table
 
+    # Create a new column that categorizes asteroids into different brightness categories based on their absolute magnitude (H)
+    df = df.withColumn("abs_mag_category", when(col("H") < 15, "Very Bright")
+        .when((col("H") >= 15) & (col("H") < 20), "Moderately Bright")
+        .otherwise("Dim"))
+
+    # Create a new column that categorizes asteroids into different orbit class categories based on their orbit class
+    df = df.withColumn("orbit_class_category", 
+                   when(df.orbit_id.startswith("M"), "Main Belt")
+                   .when(df.orbit_id.startswith("O"), "Outer Solar System")
+                   .when(df.orbit_id.startswith("I"), "Inner Solar System")
+                   .otherwise("Unknown"))
+
+    # Create a new column that categorizes asteroids into different size categories based on their diameter (km)
+    df = df.withColumn("size_category", 
+                   when(df.diameter < 1, "Small")
+                   .when((df.diameter >= 1) & (df.diameter <= 10), "Medium")
+                   .when(df.diameter > 10, "Large")
+                   .otherwise("Unknown"))
+    
     # Show the resulting DataFrame
     df.show()
 
@@ -27,6 +46,8 @@ def main():
         .option("dbtable", "neo") \
         .option("user", "postgres").option("password", "admin").save()
     
+    #TODO: Write more queries to answer interesting questions about the dataset
+
     # Find the percentage of Small Bodies that are Near Earth Objects
     total_rows = df.count()
     neo_true_count = df.filter(df.neo == 'Y').count()
@@ -34,8 +55,6 @@ def main():
 
     print("Percentage of Small bodies that are NEOs:", neo_true_percent)
     print("Percentage of Small bodies that are not NEOs", 100 - neo_true_percent)
-
-    # TODO: Write queries to answer interesting questions about the dataset
 
 if __name__ == '__main__':
     main()
