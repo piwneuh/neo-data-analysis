ffc870ee77053f4326c287cb9361e6b57c5fde6c
Filip Pinjuh <filip.pinjuh@gmail.com>
2023-07-12 17:43:02 +0200

    fix finding na

--- R/rstudio/classification.r
+++ R/rstudio/classification.r
@@ -21,13 +21,13 @@ df <- spark_read_csv(sc, name = "neo_data", path = datasetPath, header = TRUE, i
 
 # Filter data
 df <- df %>%
-  filter(!is.na(diameter_sigma) ||
-          !is.na(albedo) ||
-          !is.na(H) ||
-          !is.na(diameter)||
-          !is.na(epoch) ||
-          !is.na(name)
-           )
+  filter(!(is.na(diameter_sigma) ||
+           is.na(albedo) ||
+           is.na(H) ||
+           is.na(diameter)||
+           is.na(epoch) ||
+           is.na(name)
+           ))
 
 # Create a new column that categorizes asteroids into different brightness categories based on their absolute magnitude (H)
 df <- df %>%
@@ -45,15 +45,16 @@ df <- df %>%
     diameter > 10 ~ "Large",
     TRUE ~ "Unknown"
   ))
+
 # H devided by 5 (needed for formula)
 df <- df %>%
-  mutate(H_devided = H / 5)
+  mutate(H_divd = H / 5)
 
 # Sanity columns
 colnames(df)
 
 # Select most relevant data for further analising
-df <- df %>% select(name, albedo, H, H_devided, diameter, diameter_sigma, epoch, brightness, size_category)
+df <- df %>% select(name, albedo, H, H_divd, diameter, diameter_sigma, epoch, brightness, size_category)
 
 # Show selected data
 kable(head(df, n = 10L),
@@ -64,13 +65,13 @@ kable(head(df, n = 10L),
 )
 
 # Data split
-df_split <- sdf_random_split(dataset, training = 0.7, test = 0.3)
+df_split <- sdf_random_split(df, training = 0.7, test = 0.3)
 
 # Check the split out
 df_split
 
 # Formula
-formula <- ((1329 * 10 ^(-H/5))/diameter)^2
+formula <- ((1329 * 10 ^(-df$H_divd))/df$diameter)^2
 
 # Regression
 model1 <- ml_logistic_regression(df_split$training, label_col = "albedo", features_col = c("diameter"))
